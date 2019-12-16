0d99284b5d2920fa8fe56cc64d54e2b592d7351c
David P. Steelman <dsteelma@umd.edu>
2019-12-16 08:49:42 -0500

    LIBSEARCH-74. Added "umd_open_url" gem dependency
    Added "umd_open_url" gem dependency, and updated code to use OpenUrl
    functionality.
    
    https://issues.umd.edu/browse/LIBSEARCH-74

--- app/searchers/quick_search/ebsco_discovery_service_api_article_searcher.rb
+++ app/searchers/quick_search/ebsco_discovery_service_api_article_searcher.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'umd_open_url'
+
 module QuickSearch
   # QuickSearch seacher for EBSCO Discovery Service (Articles)
   class EbscoDiscoveryServiceApiArticleSearcher < EbscoDiscoveryServiceApiSearcher
@@ -14,6 +16,11 @@ module QuickSearch
 
     def item_link(record)
       return get_config('doi_link') + record.eds_document_doi if record.eds_document_doi
+
+      # Return link WorldCat OpenUrl link resolver, if available
+      open_url_link = link_from_open_url(record)
+      return open_url_link if open_url_link
+
       get_config('url_link') + '&db=' + record.eds_database_id + '&AN=' + record.eds_accession_number
     end
 
@@ -27,6 +34,38 @@ module QuickSearch
 
     def get_config(key)
       QuickSearch::Engine::EBSCO_DISCOVERY_SERVICE_API_ARTICLE_CONFIG[key]
+    end
+
+    def link_from_open_url(record)
+      # Generate the link to the WorldCat OpenUrl Resolver
+      open_url_link = open_url_resolve_link(record)
+      json = UmdOpenUrl::Resolver.resolve(open_url_link)
+      link = UmdOpenUrl::Resolver.parse_response(json)
+
+      link
+    end
+
+    def open_url_resolve_link(record) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      issn = record.eds_issns&.first
+      volume = record.eds_volume
+      issue_number = record.eds_issue
+      page_start = record.eds_page_start
+      date_published = record.eds_publication_date
+
+      # Return nil if the necessary parameters weren't found.
+      return nil unless issn && volume && issue_number && page_start && date_published
+
+      open_url_resolver_service_link =
+        QuickSearch::Engine::EBSCO_DISCOVERY_SERVICE_API_ARTICLE_CONFIG['open_url_resolver_service_link']
+      open_url_wskey = QuickSearch::Engine::EBSCO_DISCOVERY_SERVICE_API_ARTICLE_CONFIG['world_cat_open_url_wskey']
+
+      b = UmdOpenUrl::Builder.new(open_url_resolver_service_link)
+      b.custom_param('wskey', open_url_wskey).issn(issn).volume(volume).start_page(page_start)
+       .publication_date(date_published)
+
+      url = b.build
+
+      url
     end
   end
 end
